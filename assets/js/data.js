7870595d02a6351429717808ed625da910d9eb9f
Arman <armanbarseghyan83@gmail.com>
2023-09-07 13:50:25 -0700

    initial commit

---
+++ assets/js/data.js
@@ -0,0 +1,44 @@
+var quizData = [
+  {
+    question: "Commonly used data tipes DO Not include:",
+    answers: ["1. string", "2. booleans", "3. alerts", "4. numbers"],
+    trueAnswer: "3. alerts",
+  },
+  {
+    question: "The condition in an if / else statement is enclosed with _____.",
+    answers: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parenthesis",
+      "4. aquare brackets",
+    ],
+    trueAnswer: "3. parenthesis",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _____.",
+    answers: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    trueAnswer: "4. all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within _____ when being assigned to variables",
+    answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    trueAnswer: "3. quotes",
+  },
+  {
+    question:
+      "Very usefull tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+    trueAnswer: "4. console.log",
+  },
+];
---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+let screen1 = document.querySelector("#screen-1");
+let screen2 = document.querySelector("#screen-2");
+let screen3 = document.querySelector("#screen-3");
+let screen4 = document.querySelector("#screen-4");
+let answerBtns = document.querySelectorAll(".answer");
+let formBtn = document.querySelector("form").children[2];
+let input = document.querySelector("form").children[1];
+let startQuizBtn = document.querySelector("#screen-1 .btn");
+let highScoreBtn = document.querySelector("nav .nav-btn");
+let timeLeft = document.querySelector("nav span");
+let goBackBtn = document.querySelector("#screen-4").children[2];
+let cLearScoreBtn = document.querySelector("#screen-4").children[3];
+let message = document.querySelector("h3");
+let olEl = document.querySelector("ol");
+
+let timer;
+
+startQuizBtn.addEventListener("click", () => {
+  timer = setInterval(() => {
+    timeLeft.textContent--;
+  }, 1000);
+
+  let index = 0;
+  screen1.style.display = "none";
+  screen2.style.display = "block";
+  screen2.children[0].textContent = quizData[0].question;
+
+  answerBtns.forEach((btn, i) => {
+    btn.textContent = quizData[0].answers[i];
+    btn.addEventListener("click", () => {
+      if (btn.textContent === quizData[index]?.trueAnswer) {
+        message.textContent = "Correct!";
+      } else {
+        message.textContent = "Wrong!";
+        timeLeft.textContent -= 10;
+      }
+
+      index++;
+      if (index === quizData.length) {
+        screen2.style.display = "none";
+        screen3.style.display = "block";
+        clearInterval(timer);
+        document.querySelector("#screen-3 span").textContent =
+          timeLeft.textContent;
+      } else if (index < quizData.length) {
+        screen2.children[0].textContent = quizData[index].question;
+        answerBtns.forEach((btn, i) => {
+          btn.textContent = quizData[index].answers[i];
+        });
+      }
+    });
+  });
+});
+
+let highScores = JSON.parse(localStorage.getItem("scores")) || [];
+
+let createHighScores = () => {
+  highScores.forEach((score) => {
+    let liEl = document.createElement("li");
+    liEl.textContent = score;
+    olEl.append(liEl);
+  });
+
+  return highScores;
+};
+
+formBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if ((input.value === "")) {
+    message.textContent = "Please enter your initials!"
+    return;
+  } else {
+    message.textContent = "";
+    screen3.style.display = "none";
+    screen4.style.display = "block";
+
+    highScores.push(`${input.value} - ${timeLeft.textContent}`);
+    highScores.sort((a, b) => (a.slice(-2) > b.slice(-2) ? -1 : 1));
+
+    localStorage.setItem("scores", JSON.stringify(highScores));
+
+    olEl.textContent = "";
+    createHighScores();
+  }
+});
+
+highScoreBtn.addEventListener("click", () => {
+  clearInterval(timer);
+  timeLeft.textContent = 75;
+
+  message.textContent = "";
+  screen1.style.display = "none";
+  screen2.style.display = "none";
+  screen3.style.display = "none";
+  screen4.style.display = "block";
+
+  if (localStorage.getItem("scores") === null) {
+    olEl.textContent = "No scores yet!";
+  }
+});
+
+goBackBtn.addEventListener("click", () => {
+  location.reload();
+});
+
+cLearScoreBtn.addEventListener("click", () => {
+  localStorage.removeItem("scores");
+
+  olEl.textContent = "No scores yet!";
+  timeLeft.textContent = 75;
+});
+
+createHighScores();
